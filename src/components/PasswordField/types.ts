31aa316bab7259246a39706a4aca6cd1077f8822
Juan rojas <davidr2998@gmail.com>
2023-12-25 15:48:17 -0500

    refactor 🔨: Refactoring requirements type
    requirements type was refactored

--- src/components/PasswordField/types.ts
+++ src/components/PasswordField/types.ts
@@ -1,10 +1,34 @@
+import {
+  hasNumberChecker,
+  hasNoConsecutiveLetterChecker,
+  hasSpecialCharChecker,
+  hasUpperCaseChecker,
+} from '../../lib/checkers';
+
 export interface PasswordFieldProps {
   passwordReqs: Requirements;
+  className?: string;
 }
 
 type Requirements = {
-  numberReq: () => RegExp;
-  specialChars?: () => RegExp;
-  upperCase?: () => RegExp;
-  noConsecutive?: () => RegExp;
+  numberReq: {
+    id: string | number;
+    description: string;
+    checker: typeof hasNumberChecker;
+  };
+  specialChars?: {
+    id: string | number;
+    description: string;
+    checker: typeof hasSpecialCharChecker;
+  };
+  upperCase?: {
+    id: string | number;
+    description: string;
+    checker: typeof hasUpperCaseChecker;
+  };
+  noConsecutive?: {
+    id: string | number;
+    description: string;
+    checker: typeof hasNoConsecutiveLetterChecker;
+  };
 };
