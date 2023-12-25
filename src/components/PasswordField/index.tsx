a80e0c58f544bc1a527feb963b8d5e5ff5b453ce
Juan rojas <davidr2998@gmail.com>
2023-12-25 16:11:17 -0500

    feat ✨: implementing password field component
    Password field component has been implemented

--- src/components/PasswordField/index.tsx
+++ src/components/PasswordField/index.tsx
@@ -1,5 +1,49 @@
+import React from 'react';
 import { PasswordFieldProps } from './types';
+import style from './PasswordField.module.css';
 
-export default function PasswordField({ passwordReqs }: PasswordFieldProps) {
-  return <div>PasswordField</div>;
+export default function PasswordField({
+  passwordReqs,
+  className,
+}: PasswordFieldProps) {
+  // const descriptions = Object.values(passwordReqs).map(
+  //   ({ description }) => description
+  // );
+
+  const [password, setPassword] = React.useState('');
+
+  const requirements = Object.values(passwordReqs);
+
+  // const checkers = Object.values(passwordReqs).map(({ checker }) => checker);
+
+  function onUserType(e: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(e.target.value);
+  }
+
+  return (
+    <article
+      className={`${className && className} ${style.password_field_container}`}
+    >
+      <aside aria-label="Insert your desired password">
+        <input
+          className={`${className}`}
+          onChange={onUserType}
+          value={password}
+          type="text"
+          name="passwordField"
+        />
+      </aside>
+      <aside
+        aria-aria-label="List of password checks"
+        className={style.checklist}
+      >
+        {requirements.map(({ id, description, checker }) => (
+          <section key={id}>
+            <span>{checker(password) ? '✅' : '❌'}</span>
+            <p>{description}</p>
+          </section>
+        ))}
+      </aside>
+    </article>
+  );
 }
