66541c3cd952fd07e9aa9c015f16a52955a31046
Juan rojas <davidr2998@gmail.com>
2023-12-25 16:12:09 -0500

    feat ✨: Added password field to app.tsx
    Password field was added to app.tsx in order to be manually tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import PasswordField from './components/PasswordField';
+import {
+  hasNoConsecutiveLetterChecker,
+  hasNumberChecker,
+  hasSpecialCharChecker,
+  hasUpperCaseChecker,
+} from './lib/checkers';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <PasswordField passwordReqs={{}} />
-    </>
+    <main className="main-container">
+      <PasswordField
+        passwordReqs={{
+          numberReq: {
+            id: crypto.randomUUID(),
+            description: 'has a number 0-9',
+            checker: hasNumberChecker,
+          },
+          specialChars: {
+            id: crypto.randomUUID(),
+            description: 'has a special char',
+            checker: hasSpecialCharChecker,
+          },
+          noConsecutive: {
+            id: crypto.randomUUID(),
+            description: 'has no consecutive letters',
+            checker: hasNoConsecutiveLetterChecker,
+          },
+          upperCase: {
+            id: crypto.randomUUID(),
+            description: 'has an upper case letter',
+            checker: hasUpperCaseChecker,
+          },
+        }}
+      />
+    </main>
   );
 }
 
